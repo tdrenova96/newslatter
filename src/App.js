23a142881eeabcdf985b52f8bda1085de959eaec
tdrenova96 <125490643+tdrenova96@users.noreply.github.com>
2023-06-09 00:21:36 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import ContactForm from './ContactForm';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='my__form'>
+      <div className='container'>
+        <h1 className='text-center'>Newsletter Sign-Up
+</h1>
+        <ContactForm />
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/ContactForm.js
@@ -0,0 +1,143 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
+import emailjs from 'emailjs-com';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm();
+  const [disabled, setDisabled] = useState(false);
+
+  // Function that displays a success toast on bottom right of the page when form submission is successful
+  const toastifySuccess = () => {
+    toast('Form sent!', {
+      position: 'bottom-right',
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      className: 'submit-feedback success',
+      toastId: 'notifyToast'
+    });
+  };
+
+  // Function called on submit that uses emailjs to send email of valid contact form
+  const onSubmit = async (data) => {
+    // Destrcture data object
+    const { name, email, subject, message } = data;
+    try {
+      // Disable form while processing submission
+      setDisabled(true);
+
+      // Define template params
+      const templateParams = {
+        name,
+        email,
+        subject,
+        message
+      };
+
+      // Use emailjs to email contact form data
+      await emailjs.send(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        templateParams,
+        process.env.REACT_APP_USER_ID
+      );
+
+      // Reset contact form fields after submission
+      reset();
+      // Display success toast
+      toastifySuccess();
+      // Re-enable form submission
+      setDisabled(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div className='ContactForm'>
+      <div className='container'>
+        <div className='row justify-content-center'>
+            <div className='col-lg-6'>
+            <div className='contactForm text-center'>
+              <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
+                {/* Row 1 of form */}
+                  <div className='col-12'>
+                    <input
+                      type='text'
+                      name='name'
+                      {...register('name', {
+                        required: {
+                          value: true,
+                          message: 'Please enter your name'
+                        },
+                        maxLength: {
+                          value: 30,
+                          message: 'Please use 30 characters or less'
+                        }
+                      })}
+                      className='form-control formInput'
+                      placeholder='Name'
+                    ></input>
+                    {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
+                  </div>
+              <div className='col-12'>
+                    <input
+                      type='email'
+                      name='email'
+                      {...register('email', {
+                        required: true,
+                        pattern:
+                          /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                      })}
+                      className='form-control formInput'
+                      placeholder='Email address'
+                    ></input>
+                    {errors.email && (
+                      <span className='errorMessage'>Please enter a valid email address</span>
+                    )}
+                  </div>
+                {/* Row 2 of form */}
+                  <div className='col-12'>
+                    <input
+                      type='number'
+                      name='Number'
+                      {...register('subject', {
+                        required: {
+                          value: true,
+                          message: 'Please enter a Number'
+                        },
+                        maxLength: {
+                          value: 75,
+                          message: 'Number cannot exceed 75 characters'
+                        }
+                      })}
+                      className='form-control formInput'
+                      placeholder='Number'
+                    ></input>
+                    {errors.subject && (
+                      <span className='errorMessage'>{errors.subject.message}</span>
+                    )}
+                  </div>
+                <button className='submit-btn' disabled={disabled} type='submit'>
+                  Submit
+                </button>
+              </form>
+            </div>
+            </div>
+            <ToastContainer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
